12fc8dc3d5eb7802ae02836c0d64c59185cc2b0e
Rafał Dąbkowski <rafdab@st.amu.edu.pl>
2017-10-28 20:28:06 +0200

    0.7 - Quartz autosave completed

--- PracowaniaProgramowania/src/main/java/Proj1/Autosave.java
+++ PracowaniaProgramowania/src/main/java/Proj1/Autosave.java
@@ -1,11 +1,34 @@
 package Proj1;
 
+import org.quartz.JobDataMap;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.JobKey;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 
 public class Autosave implements org.quartz.Job {
+    //  0/30 * * ? * * *
 
     public void execute(JobExecutionContext jobExecutionContext) throws JobExecutionException {
 
+        JobDataMap dataMap = jobExecutionContext.getJobDetail().getJobDataMap();
+        String queryList = dataMap.getString("queryList");
+
+        try {
+            saveToFile(queryList);
+        } catch (FileNotFoundException | UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Autosaved!");
+    }
+
+    private static void saveToFile(String data) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter writer = new PrintWriter("odp.txt", "UTF-8");
+            writer.println(data);
+        writer.close();
     }
 }
--- PracowaniaProgramowania/src/main/java/Proj1/Runner.java
+++ PracowaniaProgramowania/src/main/java/Proj1/Runner.java
@@ -12,19 +12,42 @@ public class Runner {
     public static void main (String[] args) throws SchedulerException {
         ArrayList<String> queryList = new ArrayList<>();
 
+        //reminder quartz
         Scheduler reminderSch = StdSchedulerFactory.getDefaultScheduler();
         reminderSch.start();
+
         JobDetail reminderJob =  JobBuilder.newJob(Reminder.class)
                 .withIdentity("reminderJob")
                 .build();
+
         Trigger reminderTrigger = TriggerBuilder.newTrigger()
+                .startNow()
                 .withIdentity("reminderTrigger")
                 .withSchedule(CronScheduleBuilder.cronSchedule("0 * 8-19 ? * MON,TUE,WED,THU,FRI *")).build();
+
         reminderSch.scheduleJob(reminderJob, reminderTrigger);
+
+        //autosave quartz
+        Scheduler autosaveSch = StdSchedulerFactory.getDefaultScheduler();
+        autosaveSch.start();
+
+        JobDetail autosaveJob =  JobBuilder.newJob(Reminder.class)
+                .withIdentity("autosaveJob")
+                .usingJobData("queryList", String.valueOf(queryList))
+                .build();
+
+        Trigger autosaveTrigger = TriggerBuilder.newTrigger()
+                .startNow()
+                .withIdentity("autosaveTrigger")
+                .withSchedule(CronScheduleBuilder.cronSchedule("0/30 * * ? * * *")).build();
+
+        autosaveSch.scheduleJob(autosaveJob, autosaveTrigger);
 
         System.out.println("Wpisz '0' lub 'stop' aby zakończyć\n");
         while(start(queryList));
 
         reminderSch.shutdown();
+        autosaveSch.shutdown();
+
     }
 }
