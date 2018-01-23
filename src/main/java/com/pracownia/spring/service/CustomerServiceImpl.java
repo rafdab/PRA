889c787a7cdf0d7884ab2b7be5908559330b264a
Rafał Dąbkowski <rafdab@st.amu.edu.pl>
2018-01-23 15:00:23 +0100

    create services

---
+++ src/main/java/com/pracownia/spring/service/CustomerServiceImpl.java
@@ -0,0 +1,37 @@
+package com.pracownia.spring.service;
+
+import com.pracownia.spring.entities.Customer;
+import com.pracownia.spring.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+
+public class CustomerServiceImpl implements CustomerService{
+
+    @Autowired
+    public CustomerRepository customerRepository;
+
+    @Override
+    public Iterable<Customer> listAllCustomersPaging(Integer pageNr, Integer count) {
+        return customerRepository.findAll((Iterable<Integer>) new PageRequest(pageNr, count));
+    }
+
+    @Override
+    public Iterable<Customer> listAllCustomers(){
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public Customer getCustomerById(Integer id){
+        return customerRepository.findOne(id);
+    }
+
+    @Override
+    public Customer saveCustomer(Customer address) {
+        return customerRepository.save(address);
+    }
+
+    @Override
+    public void deleteCustomer(Integer id) {
+        customerRepository.delete(id);
+    }
+}
